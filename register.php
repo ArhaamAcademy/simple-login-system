51b559929a6885fc483fe7a0c98ca892f8f836a1
ArhaamAcademy <35983296+ArhaamAcademy@users.noreply.github.com>
2018-02-21 12:28:22 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once('config.php');
 if(isset($_POST['form_login'])){
 		
@@ -26,6 +25,9 @@ if(isset($_POST['form_login'])){
 			throw new Exception('Email cannot be empty.....');
 		}
 		
+		if(empty($_POST['user_type'])){
+			throw new Exception('User type cannot be empty.....');
+		}
 		
 		$num = 0;
 		
@@ -37,16 +39,11 @@ if(isset($_POST['form_login'])){
 			
 			throw new Exception('Username already exist. Please, choose another one....');
 		}
-		
-		
-		
-		
+
 		$statement = $db->prepare("insert into users (name, user_name, user_pass, user_email, user_phone) values(?,?,?,?,?)");
 		$statement->execute(array($_POST['name'],$_POST['username'],$password,$_POST['user_email'],$_POST['user_phone']));
 		
-		$success_message = "User has been Registerd Successfully.....!";
-		
-		
+		$success_message = "User has been Registerd Successfully.....!";	
 	
 	}
 		catch(Exception $e){
@@ -55,5 +52,50 @@ if(isset($_POST['form_login'])){
 
 }
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Simple Login System :: PHP</title>
+    </head>
+    <body>
+        <div class="form_message">
+            <?php 
+
+		if(isset($error_message)){
+		echo "<div class=' text-danger message message_warning'>".$error_message."</div>";
+
+		}
+
+		if(isset($success_message)){
+		echo "<div class='text-success message message_success'>".$success_message."</div>";
+		}
+
+	    ?>
+        </div>
+        <div class="login_form">
+            <form action="" method="post">
+		<h4>Name :</h4>
+		<input type="text" class="form-control" name="name" placeholder="Name"/>
+		<h4>Usernam :</h4>
+		<input type="text" class="form-control" name="username" placeholder="Username"/>
+		<h4>Password : </h4>
+		<input type="password" class="form-control" name="password" placeholder="Password"/>
+		<h4>Email :</h4>
+		<input type="text" class="form-control" name="user_email" placeholder="Email"/>
+		<h4>Contact :</h4>
+		<input type="text" class="form-control" name="user_phone" placeholder="Phone Number Optional"/>
+		<h4>User Type :</h4>
+		<select class="form-control" name="user_type">
+		<option value="1">Admin</option>
+		<option value="2">User</option>
+		</select>
+		<br/>
+		<input type="submit" class="btn btn-success" value="Register" name="form_login"/>
+		<a href="login.php">Back to login Page</a>
+	    </form>
+        </div>
+    </body>
+</html>
